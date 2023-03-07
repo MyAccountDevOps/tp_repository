dfe32aef5319fb37274b144e0d23873678c7d33b
MyAccountDevOps <imaneimane20201@gmail.com>
2023-03-07 17:12:33 +0100

    commit_5

--- devopstp.py
+++ devopstp.py
@@ -1,10 +1,10 @@
 def print_function() :
     print("helllo world")
 
-# def function_1(a,b):
-#     d = a + b
-#     return d
+def function_1(a,b):
+    d = a + b
+    return d
 
-# def function_2(a,b):
-#     d = a - b
-#     return d
+def function_2(a,b):
+    d = a - b
+    return d
