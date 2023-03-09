4331ae8930cd90b7a208cae3b994b90ea55361e0
MyAccountDevOps <imaneimane20201@gmail.com>
2023-03-09 22:23:59 +0100

    coommiitt

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
-import calculator
+import Calculator
 
 def test_print():
-    assert calculator.print_function("hello")  
+    assert Calculator.print_function("hello")  
 
 def test_fun1():
-    assert calculator.function_1(2, 3)== 5
+    assert Calculator.function_1(2, 3)== 5
 
 def test_fun1():
-    assert calculator.function_2(5, 2)== 3
+    assert Calculator.function_2(5, 2)== 3
