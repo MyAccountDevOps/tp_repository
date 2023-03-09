1da4d80186123f02a999dee6219a401ed1333a6b
MyAccountDevOps <imaneimane20201@gmail.com>
2023-03-09 23:00:12 +0100

    commit

--- devopstp.py
+++ devopstp.py
@@ -1,10 +1,12 @@
-def print_function(d) :
-    print(d)
+"""add tow numbers"""
+def print_function(print_smthng) :
+    """print somthing """
+    print(print_smthng)
 
-def function_1(a,b):
-    d = a + b
-    return d
+def function_1(numb_1,numb_2):
+    """add tow numbers"""
+    return numb_1 + numb_2
 
-def function_2(a,b):
-    d = a - b
-    return d
+def function_2(numb_1,numb_2):
+    """sus tow numbers"""
+    return numb_1 - numb_2
--- tests.py
+++ tests.py
@@ -1,10 +1,14 @@
+"""import calcul"""
 import Calculator
 
 def test_print():
-    assert Calculator.print_function("hello")  
+    """test_print"""
+    assert Calculator.print_function("hello") 
 
 def test_fun1():
+    """test_fun1"""
     assert Calculator.function_1(2, 3)== 5
 
-def test_fun1():
+def test_fun2():
+    """test_fun2"""
     assert Calculator.function_2(5, 2)== 3
