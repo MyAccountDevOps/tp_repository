0e2311edd88ae0e85d639439d925bf185e0fa9c4
MyAccountDevOps <imaneimane20201@gmail.com>
2023-03-13 14:51:47 +0100

    commiittt

--- devopstp.py
+++ devopstp.py
@@ -1,13 +1,17 @@
 """add tow numbers"""
-def print_function(print_smthng) :
-    """print somthing """
-    print(print_smthng)
-
-def function_1(numb_1,numb_2):
-    """add tow numbers"""
-    return numb_1 + numb_2
-
-def function_2(numb_1,numb_2):
-    """sus tow numbers"""
-    return numb_1 - numb_2
-    
+class TpClass :
+    """main class"""
+    def __init__(self):
+        pass
+    @classmethod
+    def print_function(self,print_smthng) :
+        """print somthing """
+        print(print_smthng)
+    @classmethod
+    def function_1(self,numb_1,numb_2):
+        """add tow numbers"""
+        return numb_1 + numb_2
+    @classmethod
+    def function_2(self,numb_1,numb_2):
+        """sus tow numbers"""
+        return numb_1 - numb_2
--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
-"""import calcul"""
-import Calculator
+"""import TpClass"""
+from devopstp import TpClass
 
 def test_print():
     """test_print"""
-    assert Calculator.print_function("hello") 
+    assert TpClass.print_function("hello")
 
 def test_fun1():
     """test_fun1"""
-    assert Calculator.function_1(2, 3)== 5
+    assert TpClass.function_1(2,3) == 5
 
 def test_fun2():
     """test_fun2"""
-    assert Calculator.function_2(5, 2)== 3
+    assert TpClass.function_2(5,2) == 3
