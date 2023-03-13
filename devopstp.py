a3092cec54bc71ca18f47b851c47e5c7dac250d9
MyAccountDevOps <imaneimane20201@gmail.com>
2023-03-13 14:56:07 +0100

    commiitt1

--- devopstp.py
+++ devopstp.py
@@ -4,14 +4,14 @@ class TpClass :
     def __init__(self):
         pass
     @classmethod
-    def print_function(self,print_smthng) :
+    def print_function(cls,print_smthng) :
         """print somthing """
         print(print_smthng)
     @classmethod
-    def function_1(self,numb_1,numb_2):
+    def function_1(cls,numb_1,numb_2):
         """add tow numbers"""
         return numb_1 + numb_2
     @classmethod
-    def function_2(self,numb_1,numb_2):
+    def function_2(cls,numb_1,numb_2):
         """sus tow numbers"""
         return numb_1 - numb_2
